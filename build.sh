c1cd1f1ae508d41fc473a92cff798a36069e753b
kamiorinn <snowsdream@live.com>
2020-04-06 03:09:07 +0800

    Example of build

--- build.sh
+++ build.sh
@@ -7,8 +7,11 @@ export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=../toolchain/gcc/bin/aarch64-linux-androidkernel-
 make O=./out ARCH=arm64 vendor/alphalm_lao_com-perf_defconfig
 make O=./out ARCH=arm64 REAL_CC=../toolchain/clang/bin/clang -j16
-cp -f ./out/arch/arm64/boot/Image ./packing/kernel
+cp -f ./out/arch/arm64/boot/Image.gz-dtb ./packing/Image.gz-dtb
 cd ./packing
+./magiskboot split Image.gz-dtb
+mv kernel_dtb dtb
+./magiskboot dtb dtb patch
 ./magiskboot hexpatch ./kernel \
 736B69705F696E697472616D667300 \
 77616E745F696E697472616D667300
