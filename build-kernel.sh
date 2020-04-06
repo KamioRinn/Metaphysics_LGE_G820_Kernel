c13a9e9e13088ec98d976be0f39c717f8bb02dc6
YakumoPANTS <591745794@qq.com>
2020-04-06 14:16:26 +0800

    new build.sh and defconfig

---
+++ build-kernel.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+sudo rm -rf ./out
+mkdir -p out
+cd ./out
+make clean && mrproper
+cd ..
+make clean && mrproper
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export GCC64_PATH=../toolchain/gcc/bin/aarch64-linux-androidkernel-
+make O=./out ARCH=arm64 vendor/alphalm_lao_com-perf_defconfig
+cp defconfig ./out/.config
+make O=./out ARCH=arm64 REAL_CC=../toolchain/clang/bin/clang -j8
+cp -f ./out/arch/arm64/boot/Image.gz-dtb ./packing/Image.gz-dtb
+cd ./packing
+./magiskboot split Image.gz-dtb
+mv kernel_dtb dtb
+./magiskboot dtb dtb patch
+./magiskboot hexpatch ./kernel \
+736B69705F696E697472616D667300 \
+77616E745F696E697472616D667300
+./magiskboot repack ./sample.img
+
---
+++ build-menuconfig.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mkdir -p out
+cd ./out
+make clean
+cd ..
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export GCC64_PATH=../toolchain/gcc/bin/aarch64-linux-androidkernel-
+make O=./out ARCH=arm64 vendor/alphalm_lao_com-perf_defconfig
+make O=./out menuconfig
+cp ./out/.config defconfig
+
