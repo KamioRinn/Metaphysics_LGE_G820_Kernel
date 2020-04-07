bcb322b98950e8ccb05e17de040a5e32a25d888c
YakumoPANTS <49515700+YakumoPANTS@users.noreply.github.com>
2020-04-06 19:56:44 -0500

    fix last commit

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -2002,6 +2002,9 @@ const char *irq_src;
 	}
 
 	glink->irq = irq;
+ret = enable_irq_wake(glink->irq);
+	if (ret)
+		dev_err(dev, "failed to set irq wake\n");
 
 	size = of_property_count_u32_elems(dev->of_node, "cpu-affinity");
 	if (size > 0) {
