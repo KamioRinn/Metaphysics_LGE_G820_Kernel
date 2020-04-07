f5b3a2f9491be1fb336ce83368124db39caae79c
YakumoPANTS <591745794@qq.com>
2020-04-07 09:28:04 +0800

    update defconfig

--- build-kernel.sh
+++ build-kernel.sh
@@ -7,9 +7,10 @@ cd ..
 make clean && mrproper
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export GCC64_PATH=../toolchain/gcc/bin/aarch64-linux-androidkernel-
+export CROSS_COMPILE=../toolchain/gcc/bin/aarch64-linux-androidkernel-
 make O=./out ARCH=arm64 vendor/alphalm_lao_com-perf_defconfig
 cp defconfig ./out/.config
-make O=./out ARCH=arm64 REAL_CC=../toolchain/clang/bin/clang -j8
+make O=./out ARCH=arm64 REAL_CC=../toolchain/clang/bin/clang -j12
 cp -f ./out/arch/arm64/boot/Image.gz-dtb ./packing/Image.gz-dtb
 cd ./packing
 ./magiskboot split Image.gz-dtb
--- build-menuconfig.sh
+++ build-menuconfig.sh
@@ -5,6 +5,7 @@ make clean
 cd ..
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export GCC64_PATH=../toolchain/gcc/bin/aarch64-linux-androidkernel-
+export CROSS_COMPILE=../toolchain/gcc/bin/aarch64-linux-androidkernel-
 make O=./out ARCH=arm64 vendor/alphalm_lao_com-perf_defconfig
 make O=./out menuconfig
 cp ./out/.config defconfig
