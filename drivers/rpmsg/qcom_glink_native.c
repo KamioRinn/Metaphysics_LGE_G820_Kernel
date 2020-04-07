94ef9cd7c06760170fa7e9ecc576bd8abc256f92
YakumoPANTS <49515700+YakumoPANTS@users.noreply.github.com>
2020-04-06 19:55:45 -0500

    rpmsg: glink: Specify source in IRQ name
    Currently, glink-native interrupts from all sources use the same generic
    name and are only distinguishable by number.  To improve diagnosability,
    this change creates a unique name for each glink-native IRQ that also
    specifies its source.
    
    Bug: 138658369
    Test: Ran device, exercised glink-native interrupts, verified naming in
    wakeup logs.

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -144,6 +144,7 @@ struct qcom_glink {
 	struct qcom_glink_pipe *tx_pipe;
 
 	int irq;
+const char *irq_name;
 
 	struct kthread_worker kworker;
 	struct task_struct *task;
@@ -1923,12 +1924,14 @@ struct qcom_glink *qcom_glink_native_probe(struct device *dev,
 					   struct qcom_glink_pipe *tx,
 					   bool intentless)
 {
+static const char *unknown_irq = "unknown";
+	static const char *irq_prefix = "glink-native-";
 	struct qcom_glink *glink;
 	u32 *arr;
 	int size;
 	int irq;
 	int ret;
-
+const char *irq_src;
 	glink = devm_kzalloc(dev, sizeof(*glink), GFP_KERNEL);
 	if (!glink)
 		return ERR_PTR(-ENOMEM);
@@ -1955,6 +1958,14 @@ struct qcom_glink *qcom_glink_native_probe(struct device *dev,
 	ret = of_property_read_string(dev->of_node, "label", &glink->name);
 	if (ret < 0)
 		glink->name = dev->of_node->name;
+	irq_src = glink->name;
+	if (irq_src == NULL)
+		irq_src = unknown_irq;
+	size = strlen(irq_prefix) + strlen(irq_src) + 1;
+	glink->irq_name = devm_kzalloc(dev, size, GFP_KERNEL);
+	if (!glink->irq_name)
+		return ERR_PTR(-ENOMEM);
+	snprintf((char *)glink->irq_name, size, "%s%s", irq_prefix, irq_src);
 
 	glink->mbox_client.dev = dev;
 	glink->mbox_client.knows_txdone = true;
@@ -1984,7 +1995,7 @@ struct qcom_glink *qcom_glink_native_probe(struct device *dev,
 	ret = devm_request_irq(dev, irq,
 			       qcom_glink_native_intr,
 			       IRQF_SHARED,
-			       "glink-native", glink);
+			       glink->irq_name, glink);
 	if (ret) {
 		dev_err(dev, "failed to request IRQ\n");
 		goto unregister;
